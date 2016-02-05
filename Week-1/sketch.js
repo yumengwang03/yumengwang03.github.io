3b0ae3fe3608f7e8078b44025aff3edc6c19dcd6
yumengwang03 <wangy594@newschool.edu>
2016-02-05 00:21:34 -0500

    modify

--- Week-1/sketch.js
+++ Week-1/sketch.js
@@ -1,180 +1,83 @@
-var bullet;
+var inkDrop;
+var inkDropList = [];
 
-var avatar;
+var angle; //for changing the radius and color smoothly
 
-var enemy;
+// //reference: P5.js-Examples-Live Input
+// var input;
+// var analyzer;
+// var volume;
 
-var bulletList = [];
-
-var drawing = false;
-
-// var playerPos;
-// var eLoc;
-
-// var safeDist;
-var playerPos;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  playerPos = createVector(100,100);
 
-  //bullet = new Bullet();
-
-  // avatar = new Avatar();
-  // enemy = new Enemy();
+  angle = 0;
 
   for (var i = 0; i < 160; i++) {
-    bulletList.push(new Bullet());
+    inkDropList.push(new InkDrop());
   }
+
+  // mic = new p5.AudioIn();
+  // mic.start();
 }
 
 function draw() {
-  
-  noStroke();
-  fill(255);
-  rectMode(CENTER);
-  rect(playerPos.x, playerPos.y, 106, 106);
-  
-  //fill(255, 0,0);
- // ellipse(playerPos.x, playerPos.y, 100,100);
-  playerPos.x += 2;
-  
- // background(255, 255, 255, 20);
-
-  //enemy.display();
-  // avatar.show();
-  // enemy.show();
-  // safeDist = dist(playerPos.x, playerPos.y, eLoc.x, eLoc.y);
-
-  //enemy.move();
-
-  //weapon();
-  // bullet.display();
-  // bullet.move();
-  //var second = second();
-  //console.log(second);
-  for (var q = 0; q < bulletList.length; q++) {
-    bulletList[q].display();
-    //bulletList[q].move();
-    // console.log("move");
-
+  //background(255);
+  for (var j = 0; j < inkDropList.length; j++) {
+    inkDropList[j].display();
   }
-  //console.log(safeDist);
+  //volume = mic.getLevel();
 
 }
 
-// function mousePressed() {
-//   for (var p = 0; p < bulletList.length; p++) {
-//     //bulletList[p].display();
-//     //console.log(bulletList.length);
-//     //bulletList[p].move();
-//   }
-//   drawing = true;
-//   // drawDuration = 0;
-//   // drawDuration = second();
-//   // console.log(second);
-// }
+function mousePressed() {
+  background(255);
+}
 
-function Bullet() {
-  var loc = createVector(0, 0);
-  var vel = createVector(10, 10);
-  var sizeB = random(8, 14);
-  var origin = createVector(0, 0);
+function InkDrop() {
+  var loc = createVector(30, 30);
+  var dropSize = random(8, 14);
+  var origin = createVector(30, 30);
 
   this.display = function() {
     noStroke();
-    fill(random(20, 160), 20, 255, 40);
-
-    var radius = 25;
+    //fill(random(20, 160), 20, 255, 40);
+    //var radius = 50;
+    var radius = abs(60 * sin(angle));
+    var redPoints = map(radius, 0, 50, 80, 235);
+    var greenPoints = map(radius, 0, 50, 20, 20);
+    var bluePoints = map(radius, 0, 50, 255, 235);
+    angle += 0.0001;
+    fill(redPoints, greenPoints, bluePoints, 40);
+    //console.log(radius);
+    //origin.x = mouseX;
     origin.x = mouseX;
     origin.y = mouseY;
 
-
     loc.x = random(origin.x - 2 * radius, origin.x + 2 * radius);
     loc.y = random(origin.y - 2 * radius, origin.y + 2 * radius);
-    loc.add(vel);
-    //console.log(vel);
 
     var divider = dist(loc.x, loc.y, origin.x, origin.y);
 
-    var particleSize = map(divider, 0, radius, sizeB, 1);
+    var particleSize = map(divider, 0, radius, dropSize, 1);
 
     if (divider <= radius) {
-      // loc.x += 200;
-      ellipse(loc.x, loc.y, particleSize, particleSize);
-
-      // console.log(loc);
+      //ellipse(loc.x, loc.y, particleSize, particleSize);
+      rect(loc.x + random(-3, 3), loc.y + random(-3, 3), particleSize + random(-2, 2), particleSize + random(-2, 2));
+      polygon(loc.x, loc.y, particleSize, int(random(3, 12)));
     }
-
-  }
-
-  this.move = function() {
-    vel.x += 10;
-    vel.y += 10;
-
-    //loc.add(vel);
-    //console.log(vel);
-  }
-
-  this.collide = function() {
-
-  }
-
-  this.reset = function() {
-
-  }
+  };
 }
 
-function weapon() {
-  for (var i = 0; i < 80; i++) {
-    bulletList.push(new Bullet());
-    bulletList[i].display();
+//function reference: https://processing.org/examples/regularpolygon.html
+function polygon(x, y, r, npoints) {
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * r;
+    var sy = y + sin(a) * r;
+    vertex(sx, sy);
   }
-  // for (var q = 0; q < bulletList.length; q++) {
-  //   bulletList[q].display();
-  //   //bulletList[q].move();
-  //   // console.log("move");
-  // }
-
+  endShape(CLOSE);
 }
-
-// function Enemy(mode) {
-//   //eLoc = createVector(0, 0);
-//   //var eLocX = eLoc.x;
-//   //var eLocY = eLoc.y;
-//   var eVel = createVector(0, 0);
-//   var eSize = 50;
-
-//   this.show = function() {
-//     //eVel = (2, 2);
-//     if (safeDist > 100) {
-//       //eVel = (5, 5);
-//       eLoc.x = lerp(eLoc.x, playerPos.x, .1);
-//       eLoc.y = lerp(eLoc.y, playerPos.y, .1);
-//       console.log(eLoc);
-//     } else {
-//       //eVel = (0,0);
-//     }
-//     //}
-
-//     //this.display = function() {
-//     eLoc = createVector(600, 300);
-//     noStroke();
-//     fill(20, 255, 95);
-//     ellipse(eLoc.x, eLoc.y, eSize, eSize);
-//   }
-// }
-
-
-
-// function Avatar() {
-//   this.show = function() {
-//     playerPos = createVector(0.05 * mouseX + 0.95 * pmouseX, 0.05 * mouseY + 0.95 * pmouseY);
-//     //var playerPosX = playerPos.x;
-//     //var playerPosY = playerPos.y;
-//     var playerSize = 100;
-//     noStroke();
-//     fill(105, 20, 255);
-//     ellipse(playerPos.x, playerPos.y, playerSize, playerSize);
-//   }
-// }
