14141245d06568d0dbad0495f2096dc121fe7dc9
yumengwang03 <wangy594@newschool.edu>
2016-03-26 02:59:39 -0400

    ~

--- 06/sketch.js
+++ 06/sketch.js
@@ -1,7 +1,6 @@
 // reference:
 // Bryan's concordance example - https://github.com/whoisbma/Code-2-SP16/blob/master/week-06-concordance/concordance/sketch.js
-// 2 games - Solarmax2 and Gravity Ghost
-
+// tf-idf - http://www.cnblogs.com/biyeymyhjob/archive/2012/07/17/2595249.html
 
 
 var worldSize;
@@ -13,16 +12,24 @@ var win;
 
 var testData;
 
+var keys1;
+var keys2;
+
+var wordInDoc1;
+var wordInDoc2;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   worldSize = 10;
 
-  testData1 = "HEY, REMEMBER WHEN the FCC reassured us last year that it wasn’t going to lock down Wi-Fi routers? And everyone breathed a sigh of relief, because custom router firmware is actually a really good thing? Sure, it’s fun to improve your router by extending the range or making your network friendlier for guests. But open firmware is important for other reasons: it enables critical infrastructure, from emergency communications for disaster relief and building free community access points to beefing up personal security. Well, there goes that. Because even though the FCC said its new requirements were not intended to lock down router software or block the installation of open source firmware, at least one large manufacturer has reacted by doing just that. And more could follow. Way to go, FCC. Last month, Libre Planet—a free software community—raised the alarm that TP-Link, one of the largest router manufacturers, had begun locking down firmware in newly released routers. As proof, Libre Planet pointed to a transcript of a support conversation. In the chat, a TP-Link rep says that the lockdown—which blocks the installation of open source firmware—was a reaction to new FCC requirements. That’s a problem, because alternative router software packages like DD-WRT are hugely popular. These tools provide more sophisticated features and faster security patches than manufacturers offer.";
+  testData1 = "Hey, remember when the FCC reassured us last year that it wasn’t going to lock down Wi-Fi routers? And everyone breathed a sigh of relief, because custom router firmware is actually a really good thing? Sure, it’s fun to improve your router by extending the range or making your network friendlier for guests. But open firmware is important for other reasons: it enables critical infrastructure, from emergency communications for disaster relief and building free community access points to beefing up personal security. Well, there goes that. Because even though the FCC said its new requirements were not intended to lock down router software or block the installation of open source firmware, at least one large manufacturer has reacted by doing just that. And more could follow. Way to go, FCC. Last month, Libre Planet—a free software community—raised the alarm that TP-Link, one of the largest router manufacturers, had begun locking down firmware in newly released routers. As proof, Libre Planet pointed to a transcript of a support conversation. In the chat, a TP-Link rep says that the lockdown—which blocks the installation of open source firmware—was a reaction to new FCC requirements. That’s a problem, because alternative router software packages like DD-WRT are hugely popular. These tools provide more sophisticated features and faster security patches than manufacturers offer.";
   testData2 = "We are a hybrid research group, working at the intersection of technology, culture and education. We build user-focused tools, public space interventions and forward-looking prototypes in the service of understanding and humanizing complex data systems. Currently, we’re helping to protect elephant populations in Africa, creating a space for St. Louis residents to examine the lived geographies of their city, engaging citizen scientists to connect joint pain with weather conditions, and examining the reach of one of the world's largest social networks. In the past you may have caught us explaining viral predictivity with the poop emoji, visualizing the resonance of Einstein's theory of general relativity, publishing a journal, performing MoMA's 120,000 object collections database, mapping ambulances in rural India, visualizing the history of space travel in NASA's own words, empowering individuals to reverse engineer ad targeting.";
-  // var text1 = textAnalyze(testData1);
-  // var text2 = textAnalyze(testData2);
 
-  textCompare();
+  keys1 = textAnalyze(testData1).keys;
+  keys2 = textAnalyze(testData2).keys;
+  
+  var test1 = get_tf_idf(keys1, testData1);
+
 
   for (var i = 0; i < worldSize; i++) {
     stars.push(new Star(random(150, windowWidth - 150), random(150, windowHeight - 150), floor(random(20, 100)), floor(random(0, 4))));
@@ -37,10 +44,7 @@ function setup() {
 function textAnalyze(text) {
   var concordance = {};
   var keys = [];
-
   var tokens = text.split(/\W+/);
-  // var topWords = {};
-  // var topConcordance = [];
   var cResult = {};
 
   for (var i = 0; i < tokens.length; i++) {
@@ -53,7 +57,7 @@ function textAnalyze(text) {
     }
   }
 
-  // console.log(concordance);
+  //console.log(Object.keys(concordance).length);
   // console.log(keys);
 
   keys.sort(function(a, b) {
@@ -62,59 +66,64 @@ function textAnalyze(text) {
 
   for (var i in keys) {
     if (keys.hasOwnProperty(i)) {
-      //console.log(keys[i] + " " + concordance[keys[i]]);
-      // if (topConcordance.length < worldSize) {
-      //   topConcordance.push(concordance[keys[i]]);
-      // }
-      // topWords[keys[i]] = concordance[keys[i]];
-
-      var _tf_idf = tf_idf(concordance[keys[i]], tokens.length, 2, 2);
-      //console.log(keys[i] + ' ' + _tf_idf);
-
-
       cResult[keys[i]] = concordance[keys[i]];
     }
   }
-  // console.log(topConcordance);
-  // console.log(Object.keys(topWords).length);
-  return keys;
+  return {
+    keys,
+    concordance,
+    tokens
+    // result: cResult
+  };
+}
+
+function wordInDoc(keys) {
+  keys.sort();
+  var allKeys = [];
+  var duplicated = {};
+  var concordance0 = {};
+
+  allKeys = keys1.concat(keys2);
+  
+  for (var i = 0; i < allKeys.length; i++) {
+    if (allKeys[i] in duplicated) {
+      duplicated[allKeys[i]] += 1;
+    } else {
+      duplicated[allKeys[i]] = 1;
+    }
+    for (var j = 0; j < keys.length; j++) {
+      if (allKeys[i] === keys[j]) {
+        concordance0[keys[j]] = duplicated[allKeys[i]];
+      }
+    }
+  }
+  //console.log(Object.keys(concordance0).length);
+  return concordance0;
+}
+
+function get_tf_idf(keys, text) {
+  var _wordInDocs = wordInDoc(keys);
+  var all_tf_idf = [];
+  var _concordance = textAnalyze(text).concordance;
+  var _tokens = textAnalyze(text).tokens;
+  for (var i in keys) {
+    if (keys.hasOwnProperty(i)) {
+      all_tf_idf[i] = tf_idf(_concordance[keys[i]], _tokens.length, 2, _wordInDocs[keys[i]]);
+      console.log(keys[i] + " " + all_tf_idf[i]);
+    }
+  }
+
+  return all_tf_idf;
 }
 
 function tf_idf(word, totalWords, totalDocs, wordDocs) {
   var tf = word / totalWords;
-  var idf = Math.log(totalDocs / (wordDocs + 1));
+  var idf = Math.log(totalDocs / wordDocs);
   var tf_idf = tf * idf;
 
   return tf_idf;
 }
 
-function textCompare() {
-  var keys1 = textAnalyze(testData1);
-  var keys2 = textAnalyze(testData2);
-  //console.log(keys1);
-
-  for (var i in keys1) {
-    if (keys1.hasOwnProperty(i)) {
-      for (var j in keys2) {
-        if (keys2.hasOwnProperty(j)) {
-          if (keys1[i] == keys2[j]) {
-            console.log(keys1[i] + " " + true);
-          } else {
-            console.log(false);
-          }
-        }
-      }
-      //console.log(keys1[i]);
-    }
-  }
-}
-
-
-
-
-// function checkWords() {
-//   if()
-// }
 
 function draw() {
   background(255);
@@ -123,7 +132,6 @@ function draw() {
   for (var i = 0; i < stars.length; i++) {
     stars[i].move();
     stars[i].show();
-    //stars[i].collide();
   }
 
   player.move();
